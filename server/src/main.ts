6ed1fd2e837a424f55309b4c1dc1d56c6d67ef4f
Sayyam Mehra <sayyammehra@gmail.com>
2024-12-27 01:34:11 +0530

    Updated cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,15 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  app.enableCors()
+  app.enableCors({
+    origin: [
+      'https://north-ladder-challenge.vercel.app',
+      'http://localhost:3000',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  })
+
   app.useGlobalPipes(new ValidationPipe())
 
   const config = new DocumentBuilder()
@@ -16,6 +24,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config)
   SwaggerModule.setup('api', app, document)
 
-  await app.listen(process.env.PORT || 3002)
+  await app.listen(process.env.PORT || 3000)
 }
 bootstrap()
